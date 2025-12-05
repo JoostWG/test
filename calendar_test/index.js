59c357f8a4e38c0a99d58c240544e55a28131e2e
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-12-05 22:26:00 +0100

    test

--- calendar_test/index.js
+++ calendar_test/index.js
@@ -1,18 +1,41 @@
 import dayjs from 'dayjs';
-import ical, { ICalCalendarMethod } from 'ical-generator';
+import ical from 'ical-generator';
 import fs from 'node:fs';
+import isoWeek from 'dayjs/plugin/isoWeek.js';
+
+dayjs.extend(isoWeek);
 
 const calendar = ical({ name: 'Epic calendar' });
 
-const now = dayjs();
 
-calendar.createEvent({
-    start: now.toDate(),
-    end: now.add(1, 'day').toDate(),
-    summary: 'Example Event',
-    description: 'It works ;)',
-    location: 'my room',
-    url: 'http://google.com/',
-});
+
+for (let m = 0; m < 12; m++) {
+    let d = dayjs().year(2026).month(m).startOf('month');
+
+    while (d.isoWeekday() !== 1) {
+        d = d.add(1, "day");
+    }
+
+    const start = d.hour(12).minute(0).second(0);
+    const end = start.add(1, 'minute').add(26, 'seconds');
+
+    calendar.createEvent({
+        start: start.toDate(),
+        end: end.toDate(),
+        summary: 'Luchtalarm',
+        // description: 'It works ;)',
+        // location: 'my room',
+        // url: 'http://google.com/',
+    });
+}
+
+// calendar.createEvent({
+//     start: now.toDate(),
+//     end: now.add(1, 'day').toDate(),
+//     summary: 'Example Event',
+//     description: 'It works ;)',
+//     location: 'my room',
+//     url: 'http://google.com/',
+// });
 
 fs.writeFile('./calendar.ics', calendar.toString(), () => { });
